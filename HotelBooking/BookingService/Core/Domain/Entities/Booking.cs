999154c770f3c5554c381025e344be3627711450
Jeferson Macedo <jhef.salles@gmail.com>
2025-02-28 12:36:09 -0300

    Adiciona enum Action e lógica de mudança de estado usando o partner state machine
    Implementa novas funcionalidades na classe Booking para alterar o estado de reservas com base em ações definidas no novo enum Action. As ações incluem Pay, Finish, Cancel, Refund e Reopen, com lógica de transição de estado utilizando expressão switch.

--- HotelBooking/BookingService/Core/Domain/Entities/Booking.cs
+++ HotelBooking/BookingService/Core/Domain/Entities/Booking.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Enums;
+using Action = Domain.Enums.Action;
 
 namespace Domain.Entities;
 
@@ -12,5 +13,17 @@ public class Booking
     public Status CurrentStatus
     {
         get { return this.Status; }
+    }
+    public void ChangeState(Action action)
+    {
+        this.Status = (this.Status, action) switch
+        {
+            (Status.Created, Action.Pay) => Status.Paid,
+            (Status.Created, Action.Cancel) => Status.Canceled,
+            (Status.Paid, Action.Finish) => Status.Finished,
+            (Status.Paid, Action.Refund) => Status.Refunded,
+            (Status.Canceled, Action.Reopen) => Status.Created,
+            _ => this.Status
+        };
     }
 }
